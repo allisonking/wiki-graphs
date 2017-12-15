968a00d30e58a77e0f740b890723754825aab730
Allison King <allisonjuliaking@gmail.com>
2017-12-14 23:05:52 -0500

    serve up aa lit chart with data passed from flask

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+import os, json
+from flask import render_template
 from app import wikigraph
+
+wikigraph.config.from_pyfile('config_file.cfg')
 
 @wikigraph.route('/')
 @wikigraph.route('/index')
 def index():
-    return 'Welcome!'
+    filename = os.path.join(wikigraph.static_folder, 'author_data.json')
+    with open(filename) as blog_file:
+        data = json.load(blog_file)
+    return render_template("index.html", data=data, nyt_api_key=wikigraph.config['NYT_API_KEY'])
